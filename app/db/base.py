23d1f65afad6cff8e82875a683aae768a85a604d
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-28 19:46:31 -0600

    chore: import models and base in base.py

---
+++ app/db/base.py
@@ -0,0 +1,4 @@
+from app.db.base_class import Base as Base
+from app.models.user import User as User
+from app.models.hall import Hall as Hall
+from app.models.reservation import Reservation as Reservation
