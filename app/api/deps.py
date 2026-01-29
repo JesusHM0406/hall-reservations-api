49c7d3863c38ae82225717b4c54095cb30c95a79
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-29 12:33:47 -0600

    refactor: update get_db dependency

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,5 +1,9 @@
-from app.db.session import AsyncSessionLocal
+from fastapi import Depends
+from typing import AsyncGenerator, Annotated
+from app.db.session import AsyncSession, AsyncSessionLocal
 
-async def get_db():
-  async with AsyncSessionLocal() as db:
-    yield db
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+  async with AsyncSessionLocal() as session:
+    yield session
+
+DBDep = Annotated[AsyncSession, Depends(get_db)]
