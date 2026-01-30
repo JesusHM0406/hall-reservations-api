135c87e9693517cbd9d37cb584856bb800b4c96d
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 16:19:02 -0600

    feat(routes): add basic endpoint to update a hall

--- app/api/v1/routes/hall.py
+++ app/api/v1/routes/hall.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, HTTPException
 
 from app.api.deps import DBDep
-from app.schemas.hall import HallCreate, HallRead
-from app.services.hall import service_create_new_hall, service_get_hall_by_id, service_get_hall_by_name
+from app.schemas.hall import HallCreate, HallRead, HallUpdate
+from app.services.hall import service_create_new_hall, service_get_hall_by_id, service_get_hall_by_name, service_update_hall
 
 router = APIRouter()
 
@@ -32,3 +32,12 @@ async def get_hall_by_id(hall_id: int, db: DBDep) -> HallRead:
     raise HTTPException(status_code=404, detail=f"{e}")
 
   return hall
+
+@router.patch("/{hall_id}")
+async def update_hall(hall_id: int, hall: HallUpdate, db: DBDep) -> HallRead:
+  try:
+    updated_hall = await service_update_hall(db, hall.name, hall.description, hall.is_available, hall_id)
+  except ValueError as e:
+    raise HTTPException(status_code=404, detail=f"{e}")
+
+  return updated_hall
