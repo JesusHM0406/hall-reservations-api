932d2a40a02dd27ffc938bbb574d4993673a1d9c
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 14:55:29 -0600

    feat: add basic function to get a hall by name

--- app/crud/hall.py
+++ app/crud/hall.py
@@ -7,3 +7,7 @@ from app.models.hall import Hall
 async def crud_get_hall_by_id(db: AsyncSession, id: int):
   result = await db.execute(select(Hall).where(Hall.id == id))
   return result.scalar_one_or_none()
+
+async def crud_get_hall_by_name(db: AsyncSession, name: str):
+  result = await db.execute(select(Hall).where(Hall.id == name))
+  return result.scalar_one_or_none()
