dd655d0be92a3c6bf6add2b98a192ea1af4ebcfb
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-29 18:45:25 -0600

    feat: add service function to create a new user or raise exception

---
+++ app/services/user.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from app.crud.user import create_new_user
+from app.models.user import User
+
+MIN_PASSWORD_SIZE = 8
+
+async def create_user(db: AsyncSession, name: str, password: str, password_confirm: str):
+  if len(password) < MIN_PASSWORD_SIZE:
+    raise ValueError("La contraseña debe contener al menos 8 caracteres.")
+  if password != password_confirm:
+    raise ValueError("Las contraseñas no coinciden.")
+
+  async with db.begin():
+    try:
+      new_user: User = await create_new_user(db, name, password)
+    except IntegrityError:
+      raise ValueError("El nombre de usuario ya existe.")
+
+  return {"id": new_user.id, "name": new_user.name }
