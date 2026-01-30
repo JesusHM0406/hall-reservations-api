2dd18b8ff7db5dfe28be4ae75353e6990557f9e4
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 16:14:05 -0600

    feat(routes): add basic endpoint to get a hall by name

--- app/api/v1/routes/hall.py
+++ app/api/v1/routes/hall.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 
 from app.api.deps import DBDep
 from app.schemas.hall import HallCreate, HallRead
-from app.services.hall import service_create_new_hall
+from app.services.hall import service_create_new_hall, service_get_hall_by_name
 
 router = APIRouter()
 
@@ -14,3 +14,12 @@ async def create_new_hall(db: DBDep, hall: HallCreate) -> HallRead:
     raise HTTPException(status_code=409, detail=f"{e}")
 
   return new_hall
+
+@router.get("/")
+async def get_hall_by_name(name: str, db: DBDep) -> HallRead:
+  try:
+    hall = await service_get_hall_by_name(db, name)
+  except ValueError as e:
+    raise HTTPException(status_code=404, detail=f"{e}")
+
+  return hall
