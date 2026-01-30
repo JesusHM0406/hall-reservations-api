78e9e2b976bed407c8e145d1d503239550fb9acb
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 15:33:58 -0600

    feat(service): add basic function to update a hall

--- app/services/hall.py
+++ app/services/hall.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud.hall import crud_create_new_hall, crud_get_hall_by_id, crud_get_hall_by_name
+from app.crud.hall import crud_create_new_hall, crud_get_hall_by_id, crud_get_hall_by_name, crud_update_hall
 from app.schemas.hall import HallRead
 
 
@@ -32,3 +32,14 @@ async def service_get_hall_by_name(db: AsyncSession, name: str) -> HallRead:
       raise ValueError("Hall not found.")
 
   return HallRead(id=hall.id, name=hall.name, description=hall.description, is_available=hall.is_available)
+
+async def service_update_hall(db: AsyncSession, name: str | None, description: str | None, is_available: bool | None, id: int) -> HallRead:
+  async with db.begin():
+    hall = await crud_get_hall_by_id(db, id)
+
+    if not hall:
+      raise ValueError("Hall not found.")
+
+    updated_hall = await crud_update_hall(hall, name, description, is_available)
+
+  return HallRead(id=updated_hall.id, name=updated_hall.name, description=updated_hall.description, is_available=updated_hall.is_available)
