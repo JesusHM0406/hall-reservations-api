b0eb85853ff480fdb072fd493a92422ee385df76
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 15:24:02 -0600

    feat(service): add basic function to get a hall by id

--- app/services/hall.py
+++ app/services/hall.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.crud.hall import crud_create_new_hall, crud_get_hall_by_name
+from app.crud.hall import crud_create_new_hall, crud_get_hall_by_id, crud_get_hall_by_name
 from app.schemas.hall import HallRead
 
 
@@ -14,3 +14,12 @@ async def service_create_new_hall(db: AsyncSession, name: str, description: str,
     created_hall = await crud_create_new_hall(db, name, description, is_available)
 
   return HallRead(id=created_hall.id, name=name, description=description, is_available=is_available)
+
+async def service_get_hall_by_id(db: AsyncSession, id: int) -> HallRead:
+  async with db.begin():
+    hall = await crud_get_hall_by_id(db, id)
+
+    if not hall:
+      raise ValueError("Hall not found.")
+
+  return HallRead(id=hall.id, name=hall.name, description=hall.description, is_available=hall.is_available)
