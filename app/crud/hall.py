710feae8e5f6746af9f91b60450e971958b415b3
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 15:50:04 -0600

    fix(crud): filter by name instead of by id in get_hall_by_name function

--- app/crud/hall.py
+++ app/crud/hall.py
@@ -14,7 +14,7 @@ async def crud_get_hall_by_id(db: AsyncSession, id: int):
   return result.scalar_one_or_none()
 
 async def crud_get_hall_by_name(db: AsyncSession, name: str):
-  result = await db.execute(select(Hall).where(Hall.id == name))
+  result = await db.execute(select(Hall).where(Hall.name == name))
   return result.scalar_one_or_none()
 
 async def crud_update_hall(hall: Hall, name: str | None, description: str | None, is_available: bool | None):
