675cc838ba7370f18f07d6ea20b0c70e7aab2e90
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 14:56:09 -0600

    feat: add basic function to update a hall

--- app/crud/hall.py
+++ app/crud/hall.py
@@ -11,3 +11,13 @@ async def crud_get_hall_by_id(db: AsyncSession, id: int):
 async def crud_get_hall_by_name(db: AsyncSession, name: str):
   result = await db.execute(select(Hall).where(Hall.id == name))
   return result.scalar_one_or_none()
+
+async def crud_update_hall(hall: Hall, name: str | None, description: str | None, is_available: bool | None):
+  if name:
+    hall.name = name
+  if description:
+    hall.description = description
+  if is_available is not None:
+    hall.is_available = is_available
+
+  return hall
