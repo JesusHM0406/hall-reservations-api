7607e37f10c5596355cbbd874cb8cd101a389038
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-29 18:23:14 -0600

    feat: add user crud operations

---
+++ app/crud/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, update, delete
+from app.models.user import User
+
+async def create_new_user(db: AsyncSession, name: str, pw_hash: str):
+  new_user = User(name=name, pw_hash=pw_hash)
+  db.add(new_user)
+  return new_user
+
+async def get_user_by_id(db: AsyncSession, id: int):
+  result = await db.execute(select(User.id, User.name).where(User.id == id))
+  return result.first()
+
+async def update_user(db: AsyncSession, name: str, id: int):
+  result = await db.execute(update(User).where(User.id == id).values(name=name).returning(User.name, User.id))
+  return result.scalar().all()
+
+async def delete_user(db: AsyncSession, id: int):
+  await db.execute(delete(User).where(User.id == id))
