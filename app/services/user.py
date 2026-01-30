ed96ec6808a237cad0b7cd6e3f2fe054f021657c
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 12:24:15 -0600

    style: use english in error messages

--- app/services/user.py
+++ app/services/user.py
@@ -9,15 +9,15 @@ MIN_PASSWORD_SIZE = 8
 
 async def service_create_user(db: AsyncSession, name: str, password: str, password_confirm: str) -> UserRead:
   if len(password) < MIN_PASSWORD_SIZE:
-    raise ValueError("La contraseña debe contener al menos 8 caracteres.")
+    raise ValueError("The password must contain at least 8 characters.")
   if password != password_confirm:
-    raise ValueError("Las contraseñas no coinciden.")
+    raise ValueError("The passwords don't match.")
 
   async with db.begin():
     user = await crud_get_user_by_name(db, name)
 
     if user is not None:
-      raise ValueError("El nombre de usuario ya existe")
+      raise ValueError("The name already exists.")
 
     pw_hash = generate_password_hash(password)
     new_user: User = await crud_create_new_user(db, name, pw_hash)
@@ -28,7 +28,7 @@ async def service_get_user_by_id(db: AsyncSession, id: int) -> UserRead:
   user = await crud_get_user_by_id(db, id)
 
   if not user:
-    raise ValueError("El usuario no existe")
+    raise ValueError("User not found.")
 
   return UserRead(id=user.id, name=user.name )
 
@@ -37,7 +37,7 @@ async def service_update_user(db: AsyncSession, name: str, id: int) -> UserRead:
     user = await crud_get_user_by_id(db, id)
 
     if not user:
-      raise ValueError("El usuario no existe")
+      raise ValueError("User not found.")
 
     await crud_update_user(db, name, id)
 
@@ -48,7 +48,7 @@ async def service_delete_user(db: AsyncSession, id: int):
     user = await crud_get_user_by_id(db, id)
 
     if not user:
-      raise ValueError("El usuario que intentas eliminar no existe")
+      raise ValueError("The user you want to delete doesn't exist.")
 
     await crud_delete_user(db, id)
 
