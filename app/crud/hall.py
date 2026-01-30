e1530a81a11345167d1cf032e5145f2f2f96d3b2
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 14:56:38 -0600

    feat: add basic function to delete a hall

--- app/crud/hall.py
+++ app/crud/hall.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.hall import Hall
@@ -21,3 +21,6 @@ async def crud_update_hall(hall: Hall, name: str | None, description: str | None
     hall.is_available = is_available
 
   return hall
+
+async def crud_delete_hall(db: AsyncSession, id: int):
+  await db.execute(delete(Hall).where(Hall.id == id))
