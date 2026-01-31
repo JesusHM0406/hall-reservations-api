4f0cd79aa4c468bfa0a3246e880211366fff2be0
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 21:00:00 -0600

    feat(service): add basic function to retrieve a reservation

--- app/services/reservation.py
+++ app/services/reservation.py
@@ -4,12 +4,12 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.hall import crud_get_hall_by_id
-from app.crud.reservation import crud_create_new_reservation
+from app.crud.reservation import crud_create_new_reservation, crud_get_reservation
 from app.crud.user import crud_get_user_by_id
 from app.schemas.reservation import ReservationRead
 
 
-async def service_create_new_reservation(db: AsyncSession, user_id: int, hall_id: int, reservation_date: date):
+async def service_create_new_reservation(db: AsyncSession, user_id: int, hall_id: int, reservation_date: date) -> ReservationRead:
   async with db.begin():
     today = date.today()
     if reservation_date < today or reservation_date == today:
@@ -30,3 +30,22 @@ async def service_create_new_reservation(db: AsyncSession, user_id: int, hall_id
       return ReservationRead(id=reservation.id, user_id=user_id, user_name=user.name, hall_id=hall_id, hall_name=hall.name, status=reservation.status, reservation_date=reservation.reservation_date)
     except IntegrityError:
       raise ValueError("There's already an active reservation in that date.")
+
+async def service_get_reservation(db: AsyncSession, id: int) -> ReservationRead:
+  async with db.begin():
+    reservation = await crud_get_reservation(db, id)
+
+    if not reservation:
+      raise ValueError("Reservation not found.")
+
+    user = await crud_get_user_by_id(db, reservation.user_id)
+    if not user:
+    # This is obviously provisional.
+      raise ValueError("It appears the user was deleted.")
+
+    hall = await crud_get_hall_by_id(db, reservation.hall_id)
+    if not hall:
+    # The same
+      raise ValueError("Hall not found.")
+
+  return ReservationRead(id=reservation.id, user_id=reservation.user_id, user_name=user.name, hall_id=reservation.hall_id, hall_name=hall.name, status=reservation.status, reservation_date=reservation.reservation_date)
