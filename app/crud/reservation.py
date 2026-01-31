2fe142f207e7c2c9b0882f0e714940dd5875d319
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 19:20:49 -0600

    feat(crud): add basic function to retrieve a reservation

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from datetime import date
 
@@ -9,3 +10,7 @@ async def crud_create_new_reservation(db: AsyncSession, user_id: int, hall_id: i
   new_reservation = Reservation(user_id=user_id, hall_id=hall_id, reservation_date=reservation_date)
   db.add(new_reservation)
   return new_reservation
+
+async def crud_get_reservation(db: AsyncSession, reservation_id: int):
+  reservation = await db.execute(select(Reservation).where(Reservation.id == reservation_id))
+  return reservation.scalar_one_or_none()
