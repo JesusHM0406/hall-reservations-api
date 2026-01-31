a925e30de7991faa6ddca53cf2062b7b5a462857
Jesus HM <jesus.h.mora0406@gmail.com>
2026-01-30 19:11:23 -0600

    feat: add basic reservation schemas

---
+++ app/schemas/reservation.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from datetime import date
+
+class ReservationCreate(BaseModel):
+  user_id: int
+  hall_id: int
+  reservation_date: date
+
+class ReservationRead(BaseModel):
+  id: int
+  hall_name: str
+  status: str
+  reservation_date: date
+
+class ReservationUpdate(BaseModel):
+  status: str
