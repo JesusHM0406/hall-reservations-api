95a8593e29f9a736632b8faefcb7d75d333ca43f
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-03 12:03:49 -0600

    feat(routes): add basic endpoint to get a reservation

--- app/api/v1/routes/reservation.py
+++ app/api/v1/routes/reservation.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, HTTPException, status
 from app.api.deps import DBDep
 from app.schemas.reservation import ReservationRead, ReservationCreate
 from app.services.reservation import (
-  service_create_new_reservation
+  service_create_new_reservation,
+  service_get_reservation
 )
 
 
@@ -14,3 +15,10 @@ async def create_reservation(reservation: ReservationCreate, db: DBDep) -> Reser
     return await service_create_new_reservation(db, reservation.user_id, reservation.hall_id, reservation.reservation_date)
   except ValueError as e:
     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+
+@router.get("/{reservation_id}")
+async def get_reservation(reservation_id: int, db: DBDep) -> ReservationRead:
+  try:
+    return await service_get_reservation(db, reservation_id)
+  except ValueError as e:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
