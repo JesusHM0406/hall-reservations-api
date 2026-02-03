38d18824dd989123d54465967279145d6ea03dda
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-03 10:22:55 -0600

    feat(services): add basic funtion to update reservation status

--- app/services/reservation.py
+++ app/services/reservation.py
@@ -9,9 +9,11 @@ from app.crud.reservation import (
   crud_get_all_reservations,
   crud_get_all_reservations_by_user_id,
   crud_get_reservation,
-  crud_get_reservations_by_hall_id
+  crud_get_reservations_by_hall_id,
+  crud_update_reservation_status
 )
 from app.crud.user import crud_get_user_by_id
+from app.models.reservation_status import ReservationStatus
 from app.schemas.reservation import ReservationRead
 
 
@@ -85,7 +87,39 @@ async def service_get_all_reservations_by_hall_id(db: AsyncSession, hall_id: int
       raise ValueError("Hall not found.")
 
     result = await crud_get_reservations_by_hall_id(db, hall_id)
-  
+
   data = [ReservationRead(id=reservation.id, user_id=reservation.user_id, user_name=reservation.user.name, hall_id=hall_id, hall_name=hall.name, status=reservation.status, reservation_date=reservation.reservation_date) for reservation in result]
 
   return data
+
+async def service_update_reservation_status(db: AsyncSession, reservation_id: int, new_status: str, user_id: int) -> ReservationRead:
+  async with db.begin():
+    user = await crud_get_user_by_id(db, user_id)
+
+    if not user:
+      raise ValueError("User not found.")
+
+    reservation = await crud_get_reservation(db, reservation_id)
+
+    if not reservation:
+      raise ValueError("Reservation not found.")
+
+    hall = await crud_get_hall_by_id(db, reservation.hall_id)
+
+    if not hall:
+      raise ValueError("It appears the hall was deleted.")
+
+    if reservation.user_id != user.id:
+      raise ValueError("The reservation is not from this user.")
+
+    if reservation.status in [ReservationStatus.CANCELLED, ReservationStatus.EXPIRED]:
+      raise ValueError("You cannot modify a reservation that has already been cancelled or expired.")
+
+    if new_status not in [ReservationStatus.CANCELLED.value, ReservationStatus.CONFIRMED.value, ReservationStatus.EXPIRED.value, ReservationStatus.FINISHED.value, ReservationStatus.PENDING.value]:
+      raise ValueError("Invalid status.")
+
+    status_enum = ReservationStatus(new_status)
+
+    updated_reservation = await crud_update_reservation_status(status_enum, reservation)
+
+  return ReservationRead(id=reservation.id, user_id=user.id, user_name=user.name, hall_id=hall.id, hall_name=hall.name, status=updated_reservation.status, reservation_date=reservation.reservation_date)
