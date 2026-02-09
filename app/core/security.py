04482a0e8e1a902b64339b317918fd34c9d32f42
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-08 19:12:26 -0600

    feat: create a function to verify a password

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,6 @@
 from pwdlib import PasswordHash
 
 password_hash = PasswordHash.recommended()
+
+def verify_password(plain_password: str, hashed_password: str):
+  return password_hash.verify(plain_password, hashed_password)
