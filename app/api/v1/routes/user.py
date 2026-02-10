4288d378c37f764300613635099040e069023324
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-10 11:36:28 -0600

    feat(users): add endpoint to return the current user

--- app/api/v1/routes/user.py
+++ app/api/v1/routes/user.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 
-from app.api.deps import DBDep
+from app.api.deps import DBDep, get_current_user
 from app.models.hall import Hall as Hall
 from app.models.reservation import Reservation as Reservation
 from app.models.user import User as User
-from app.schemas.user import UserCreate, UserRead, UserUpdate
+from app.schemas.user import UserComplete, UserCreate, UserRead, UserUpdate
 from app.services.user import (
   service_create_user,
   service_delete_user,
@@ -24,6 +25,10 @@ async def add_user(user: UserCreate, db: DBDep) -> UserRead:
 async def get_all_users(db: DBDep) -> list[UserRead]:
   async with db.begin():
     return await service_get_all_users(db)
+
+@router.get("/me")
+async def route_get_current_user(user: Annotated[UserComplete, Depends(get_current_user)]) -> UserComplete:
+  return user
 
 @router.get("/{id}")
 async def get_user_by_id(id: int, db: DBDep) -> UserRead:
