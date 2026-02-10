843bf1464b848345bcfa4866442dd7a9c3fe1fb8
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-10 11:30:39 -0600

    fix: return None when no user found in crud_user_by_id

--- app/crud/user.py
+++ app/crud/user.py
@@ -10,8 +10,8 @@ async def crud_create_new_user(db: AsyncSession, name: str, pw_hash: str):
   return new_user
 
 async def crud_get_user_by_id(db: AsyncSession, id: int):
-  result = await db.execute(select(User.id, User.name).where(User.id == id))
-  return result.first()
+  result = await db.execute(select(User).where(User.id == id))
+  return result.scalar_one_or_none()
 
 async def crud_update_user(db: AsyncSession, name: str, id: int):
   await db.execute(update(User).where(User.id == id).values(name=name))
