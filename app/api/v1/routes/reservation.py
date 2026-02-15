d8f60ac9c8078d7db0d623ce751a61316817fb85
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-14 19:20:40 -0600

    feat: add correct return types to the finish and cancel endpoint functions

--- app/api/v1/routes/reservation.py
+++ app/api/v1/routes/reservation.py
@@ -45,11 +45,11 @@ async def approve_reservation(db: DBDep, admin: AdminDep, approve: ReservationAp
     return await service_update_reservation_status(db, id, ReservationStatus.CONFIRMED.value, approve.user_id)
 
 @router.patch("/{id}/finish")
-async def finish_reservation(db: DBDep, user: UserDep, id: int):
+async def finish_reservation(db: DBDep, user: UserDep, id: int) -> ReservationRead:
   async with db.begin():
     return await service_update_reservation_status(db, id, ReservationStatus.FINISHED.value, user.id)
 
 @router.patch("/{id}/cancel")
-async def cancel_reservation(db: DBDep, user: UserDep, id: int):
+async def cancel_reservation(db: DBDep, user: UserDep, id: int) -> ReservationRead:
   async with db.begin():
     return await service_update_reservation_status(db, id, ReservationStatus.CANCELLED.value, user.id)
