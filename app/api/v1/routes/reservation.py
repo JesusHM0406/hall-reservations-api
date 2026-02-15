3a4fa251dd05bdd15df471e8b7292c6966f4e7ed
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-14 18:59:56 -0600

    feat: use user and admin dependencies in the respective reservation endpoints

--- app/api/v1/routes/reservation.py
+++ app/api/v1/routes/reservation.py
@@ -1,10 +1,7 @@
-from typing import Annotated
-
-from fastapi import APIRouter, status, Depends
-from app.api.deps import DBDep, get_current_active_admin, get_current_active_user
+from fastapi import APIRouter, status
+from app.api.deps import AdminDep, DBDep, UserDep
 from app.models.reservation_status import ReservationStatus
 from app.schemas.reservation import ReservationApprove, ReservationRead, ReservationCreate, ReservationUpdate
-from app.schemas.user import UserComplete
 from app.services.reservation import (
   service_create_new_reservation,
   service_get_all_reservations,
@@ -39,11 +36,11 @@ async def get_single_reservation(id: int, db: DBDep) -> ReservationRead:
     return await service_get_reservation(db, id)
 
 @router.patch("/{id}")
-async def update_reservation_status(db: DBDep, user: Annotated[UserComplete, Depends(get_current_active_user)], update: ReservationUpdate, id: int) -> ReservationRead:
+async def update_reservation_status(db: DBDep, user: UserDep, update: ReservationUpdate, id: int) -> ReservationRead:
   async with db.begin():
     return await service_update_reservation_status(db, id, update.status, user.id)
 
 @router.patch("/{id}/approve")
-async def approve_reservation(db: DBDep, admin: Annotated[UserComplete, Depends(get_current_active_admin)], approve: ReservationApprove, id: int) -> ReservationRead:
+async def approve_reservation(db: DBDep, admin: AdminDep, approve: ReservationApprove, id: int) -> ReservationRead:
   async with db.begin():
     return await service_update_reservation_status(db, id, ReservationStatus.CONFIRMED.value, approve.user_id)
