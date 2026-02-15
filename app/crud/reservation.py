a1ea8322a9c059df524135058591dbca4d3ac2ec
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-15 16:07:59 -0600

    feat: add crud operation to return paginated reservations

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -1,12 +1,15 @@
 from datetime import date
 
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from app.models.hall import Hall
 from app.models.reservation import Reservation, ReservationStatus
 from app.models.user import User
+from app.utils.pagination_crud import PaginationCRUD
+
+LIMIT_PER_PAGE = 10
 
 async def crud_create_new_reservation(db: AsyncSession, user_id: int, hall_id: int, reservation_date: date):
   new_reservation = Reservation(user_id=user_id, hall_id=hall_id, reservation_date=reservation_date)
@@ -51,3 +54,41 @@ async def crud_get_reservations_by_hall_id(db: AsyncSession, hall_id: int):
 async def crud_update_reservation_status(new_status: ReservationStatus, reservation: Reservation):
   reservation.status = new_status
   return reservation
+
+async def crud_get_reservations(db: AsyncSession, user_filter: bool, hall_filter: bool, filter_id: int | None, last_id: int | None) -> PaginationCRUD:
+  stmt = (
+    select(Reservation)
+    .order_by(Reservation.id.desc())
+    .limit(LIMIT_PER_PAGE)
+  )
+
+  total_records_stmt = select(func.count()).select_from(Reservation)
+
+  if filter_id:
+    if user_filter:
+      stmt = stmt.where(Reservation.user_id == filter_id)
+      total_records_stmt = total_records_stmt.where(Reservation.user_id == filter_id)
+    elif hall_filter:
+      stmt = stmt.where(Reservation.hall_id == filter_id)
+      total_records_stmt = total_records_stmt.where(Reservation.hall_id == filter_id)
+
+  total_records = await db.execute(total_records_stmt)
+  total_records = total_records.scalar() or 0
+
+  if last_id:
+    stmt = stmt.where(Reservation.id < last_id)
+
+  result = await db.execute(stmt)
+  result_items = result.scalars().all()
+  data = [
+    {
+      "id": item.id,
+      "user_id": item.user_id,
+      "hall_id": item.hall_id,
+      "status": item.status.value if hasattr(item.status, 'value') else item.status,
+      "reservation_date": item.reservation_date
+    }
+    for item in result_items
+  ]
+
+  return PaginationCRUD(items=list(data), total=total_records, per_page=LIMIT_PER_PAGE)
