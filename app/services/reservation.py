ba8a8eac19255d63336bbab50e83a5473112c570
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-15 20:17:16 -0600

    feat(service): add function to get paginated reservations by hall_id

--- app/services/reservation.py
+++ app/services/reservation.py
@@ -156,3 +156,10 @@ async def service_get_all_reservations_by_user_id_paginate(db: AsyncSession, pag
   pagination = Pagination(items=result.items, requested_page=page, per_page=result.per_page, total=result.total, pages=result.pages, current_page=result.current_page)
 
   return pagination
+
+async def service_get_all_reservations_by_hall_id_paginate(db: AsyncSession, page: int, hall_id: int) -> Pagination:
+  result = await crud_get_reservations(db, page, filter_id=hall_id, hall_filter=True)
+
+  pagination = Pagination(items=result.items, requested_page=page, per_page=result.per_page, total=result.total, pages=result.pages, current_page=result.current_page)
+
+  return pagination
