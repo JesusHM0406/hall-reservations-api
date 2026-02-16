733d1adbd082450752e09c0bb8afdd64cdfba62c
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-16 11:27:02 -0600

    feat(crud): add function to return all users paginated

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,7 +1,12 @@
-from sqlalchemy import delete, select, update
+import math
+
+from sqlalchemy import delete, select, update, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.config import settings
 from app.models.user import User
+from app.utils.pagination_crud import PaginationCRUD
+from app.schemas.user import UserComplete
 
 
 async def crud_create_new_user(db: AsyncSession, name: str, pw_hash: str):
@@ -28,3 +33,33 @@ async def crud_get_all_users(db: AsyncSession):
   result = await db.execute(select(User.id, User.name))
 
   return result
+
+async def crud_get_all_users_paginate(db: AsyncSession, page: int) -> PaginationCRUD:
+  stmt = select(User).order_by(User.id.desc())
+  total_records_stmt = select(func.count()).select_from(User)
+
+
+  total_res = await db.execute(total_records_stmt)
+  total_records = total_res.scalar() or 0
+
+  pages = math.ceil(total_records / settings.PAGINATION_LIMIT_PER_PAGE) if total_records > 0 else 1
+
+  current_page = max(1, min(page, pages))
+
+  current_offset = (current_page - 1) * settings.PAGINATION_LIMIT_PER_PAGE
+
+  stmt = stmt.limit(settings.PAGINATION_LIMIT_PER_PAGE).offset(current_offset)
+
+  result = await db.execute(stmt)
+  result_items = result.scalars().all()
+  data = [
+    UserComplete(
+      id=user.id,
+      name=user.name,
+      role=user.role,
+      is_active=user.is_active
+    )
+    for user in result_items
+  ]
+
+  return PaginationCRUD(items=list(data), total=total_records, per_page=settings.PAGINATION_LIMIT_PER_PAGE, pages=pages, current_page=current_page)
