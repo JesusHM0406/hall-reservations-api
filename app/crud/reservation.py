cba0ed655b69e621d063e0236741a19c4cdb1d81
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-15 20:58:07 -0600

    refactor(crud): remove innecesary crud functions

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -3,11 +3,8 @@ import math
 
 from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
 
-from app.models.hall import Hall
 from app.models.reservation import Reservation, ReservationStatus
-from app.models.user import User
 from app.utils.pagination_crud import PaginationCRUD
 
 LIMIT_PER_PAGE = 10
@@ -20,37 +17,6 @@ async def crud_create_new_reservation(db: AsyncSession, user_id: int, hall_id: i
 async def crud_get_reservation(db: AsyncSession, reservation_id: int):
   reservation = await db.execute(select(Reservation).where(Reservation.id == reservation_id))
   return reservation.scalar_one_or_none()
-
-async def crud_get_all_reservations(db: AsyncSession):
-  stmt = (
-    select(Reservation)
-    .options(
-      joinedload(Reservation.user).load_only(User.name),
-      joinedload(Reservation.hall).load_only(Hall.name)
-    )
-  )
-
-  result = await db.scalars(stmt)
-
-  return result.all()
-
-async def crud_get_all_reservations_by_user_id(db: AsyncSession, user_id: int):
-  stmt = (
-    select(Reservation)
-    .options(
-      joinedload(Reservation.hall).load_only(Hall.name)
-    )
-    .where(Reservation.user_id == user_id)
-  )
-
-  result = await db.scalars(stmt)
-
-  return result.all()
-
-async def crud_get_reservations_by_hall_id(db: AsyncSession, hall_id: int):
-  stmt = select(Reservation).options(joinedload(Reservation.user).load_only(User.name)).where(Reservation.hall_id == hall_id)
-  result = await db.scalars(stmt)
-  return result.all()
 
 async def crud_update_reservation_status(new_status: ReservationStatus, reservation: Reservation):
   reservation.status = new_status
