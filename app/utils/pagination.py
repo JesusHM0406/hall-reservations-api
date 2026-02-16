63f706255261653f2495a90380b2cca323dbf82c
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-15 20:30:50 -0600

    feat(utils): add get_pagination function to return an Pagination instance

--- app/utils/pagination.py
+++ app/utils/pagination.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Any, List
 
+from app.utils.pagination_crud import PaginationCRUD
+
 
 @dataclass
 class Pagination:
@@ -10,3 +12,6 @@ class Pagination:
   total: int
   pages: int
   current_page: int
+
+def get_pagination(pagination: PaginationCRUD, page: int) -> Pagination:
+  return Pagination(items=pagination.items, requested_page=page, per_page=pagination.per_page, total=pagination.total, pages=pagination.pages, current_page=pagination.current_page)
