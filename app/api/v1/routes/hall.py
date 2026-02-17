a336ed099e548579e03bcf031d531e428cb21169
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-17 11:45:40 -0600

    feat: add endpoint to update hall availability

--- app/api/v1/routes/hall.py
+++ app/api/v1/routes/hall.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.deps import DBDep
-from app.schemas.hall import HallCreate, HallRead, HallUpdate
+from app.schemas.hall import HallCreate, HallRead, HallUpdate, HallUpdateAvailability
 from app.services.hall import (
   service_create_new_hall,
   service_delete_hall,
@@ -9,6 +9,7 @@ from app.services.hall import (
   service_get_hall_by_id,
   service_get_hall_by_name,
   service_update_hall,
+  service_update_hall_availability,
 )
 
 router = APIRouter()
@@ -37,6 +38,11 @@ async def get_hall_by_id(id: int, db: DBDep) -> HallRead:
 async def update_hall(id: int, hall: HallUpdate, db: DBDep) -> HallRead:
   async with db.begin():
     return await service_update_hall(db, hall.name, hall.description, hall.is_available, id)
+
+@router.patch("/{id}/availability")
+async def update_hall_availability(db: DBDep, id: int, update: HallUpdateAvailability) -> HallRead:
+  async with db.begin():
+    return await service_update_hall_availability(db, id, update.is_available)
 
 @router.delete("/{id}", status_code=204)
 async def delete_hall(id: int, db: DBDep):
