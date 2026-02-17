6acc20a0dad484aa013f20efe6c076c7269b0c97
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-17 12:27:46 -0600

    feat(crud): add temporary function to return all halls with PaginationCRUD

--- app/crud/hall.py
+++ app/crud/hall.py
@@ -1,7 +1,11 @@
-from sqlalchemy import select
+import math
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.core.config import settings
 from app.models.hall import Hall
+from app.schemas.hall import HallRead
+from app.utils.pagination_crud import PaginationCRUD
 
 
 async def crud_create_new_hall(db: AsyncSession, name: str, description: str, is_available: bool):
@@ -36,3 +40,32 @@ async def crud_get_all_halls(db: AsyncSession):
   result = await db.execute(select(Hall))
 
   return result.all()
+
+async def crud_get_all_halls_paginated(db: AsyncSession, page: int) -> PaginationCRUD:
+  stmt = select(Hall).order_by(Hall.id.desc())
+  total_records_stmt = select(func.count()).select_from(Hall)
+
+  total_res = await db.execute(total_records_stmt)
+  total_records = total_res.scalar() or 0
+
+  pages = math.ceil(total_records / settings.PAGINATION_LIMIT_PER_PAGE) if total_records > 0 else 1
+
+  current_page = max(1, min(page, pages))
+
+  current_offset = (current_page - 1) * settings.PAGINATION_LIMIT_PER_PAGE
+
+  stmt = stmt.limit(settings.PAGINATION_LIMIT_PER_PAGE).offset(current_offset)
+
+  result = await db.execute(stmt)
+  result_items = result.scalars().all()
+  data = [
+    HallRead(
+      id=hall.id,
+      name=hall.name,
+      description=hall.description,
+      is_available=hall.is_available
+    )
+    for hall in result_items
+  ]
+
+  return PaginationCRUD(items=list(data), total=total_records, per_page=settings.PAGINATION_LIMIT_PER_PAGE, pages=pages, current_page=current_page)
