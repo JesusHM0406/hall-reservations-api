fa4a0ffe2123fc3e1611de2cb102ec5e948fd1e3
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-17 18:31:45 -0600

    feat: add available_filters property to Pagination class

--- app/utils/pagination.py
+++ app/utils/pagination.py
@@ -3,6 +3,7 @@ from typing import Any, List
 from pydantic import BaseModel, computed_field
 
 from app.utils.pagination_crud import PaginationCRUD
+from app.utils.pagination_filters import AvailableFilter
 
 
 class Pagination(BaseModel):
@@ -12,6 +13,7 @@ class Pagination(BaseModel):
   per_page: int
   pages: int
   current_page: int
+  available_filters: List[AvailableFilter] = []
 
   @computed_field
   def has_prev(self) -> bool:
@@ -29,5 +31,5 @@ class Pagination(BaseModel):
   def next_num(self) -> int | None:
     return self.current_page + 1 if self.has_next else None
 
-def get_pagination(pagination: PaginationCRUD, page: int) -> Pagination:
-  return Pagination(items=pagination.items, requested_page=page, per_page=pagination.per_page, total=pagination.total, pages=pagination.pages, current_page=pagination.current_page)
+def get_pagination(pagination: PaginationCRUD, page: int, filters: List[AvailableFilter]) -> Pagination:
+  return Pagination(items=pagination.items, requested_page=page, per_page=pagination.per_page, total=pagination.total, pages=pagination.pages, current_page=pagination.current_page, available_filters=filters)
