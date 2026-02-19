92ce42e284c43090537b7d9d686429004e8d23aa
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-19 12:24:00 -0600

    feat(models): add search_vector column and indexes for search purposes

--- app/models/hall.py
+++ app/models/hall.py
@@ -1,5 +1,6 @@
 from app.db.base_class import Base
-from sqlalchemy import Integer, String, Boolean, Text as SQLtext
+from sqlalchemy import Integer, String, Boolean, Text as SQLtext, Computed, Index
+from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Text, TYPE_CHECKING, List
 
@@ -8,11 +9,24 @@ if TYPE_CHECKING:
 
 class Hall(Base):
   __tablename__ = "halls"
-  
+
   id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
   name: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
   description: Mapped[Text] = mapped_column(SQLtext, nullable=False)
   is_available: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
-  
+  search_vector: Mapped[TSVECTOR] = mapped_column(
+      TSVECTOR,
+      Computed(
+        "setweight(to_tsvector('english', name), 'A') || "
+        "setweight(to_tsvector('english', description), 'B')",
+        persisted=True
+      )
+    )
+
   # Relationships
   reservations: Mapped[List["Reservation"]] = relationship("Reservation", back_populates="hall", lazy="raise")
+
+  __table_args__ = (
+      Index("idx_room_search_vector", "search_vector", postgresql_using="gin"),
+      Index("idx_room_name_trgm", "name", postgresql_using="gist", postgresql_ops={"name": "gist_trgm_ops"}),
+    )
