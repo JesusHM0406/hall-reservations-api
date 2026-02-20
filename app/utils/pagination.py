4b23933132cce8e5b30a32217ba4457a85dfc50d
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-19 18:02:11 -0600

    feat(utils): add functionality to get the computed field from the pagination instead of repeating the code

--- app/utils/pagination.py
+++ app/utils/pagination.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass
+import math
 from typing import Any, List
 
 from pydantic import BaseModel, computed_field
 
+from app.core.config import settings
 from app.utils.pagination_crud import PaginationCRUD
 from app.utils.pagination_filters import AvailableFilter
 
@@ -31,6 +34,12 @@ class Pagination(BaseModel):
   def next_num(self) -> int | None:
     return self.current_page + 1 if self.has_next else None
 
+@dataclass
+class PaginationComputedFields():
+  pages: int
+  current_page: int
+  current_offset: int
+
 def get_pagination(*, pagination: PaginationCRUD, page: int, available_filters: List[AvailableFilter]) -> Pagination:
   return Pagination(
     items=pagination.items,
@@ -40,4 +49,23 @@ def get_pagination(*, pagination: PaginationCRUD, page: int, available_filters: 
     pages=pagination.pages,
     current_page=pagination.current_page,
     available_filters=available_filters
+  )
+
+def get_pagination_computed_fields(
+  *,
+  total_records: int,
+  page: int
+) -> PaginationComputedFields:
+  pages = math.ceil(
+    total_records / settings.PAGINATION_LIMIT_PER_PAGE
+  ) if total_records > 0 else 1
+
+  current_page = max(1, min(page, pages))
+
+  current_offset = (current_page - 1) * settings.PAGINATION_LIMIT_PER_PAGE
+
+  return PaginationComputedFields(
+    pages=pages,
+    current_page=current_page,
+    current_offset=current_offset
   )
