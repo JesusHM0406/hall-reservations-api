d27a4a65cae5cf7c9ee72af727cfee5cde151b39
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-22 13:13:08 -0600

    feat(models): add is_deleted column in User model

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,7 @@
 from typing import TYPE_CHECKING, List
 
-from sqlalchemy import Boolean, Enum, Integer, String
+from sqlalchemy import Boolean, Enum, Integer, String, false, true
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.sql import true
 
 from app.db.base_class import Base
 from app.models.user_role import UserRole
@@ -18,6 +17,7 @@ class User(Base):
   pw_hash: Mapped[str] = mapped_column(String(255), nullable=False)
   role: Mapped[UserRole] = mapped_column(Enum(UserRole, name="user_role"), default=UserRole.USER, nullable=False)
   is_active: Mapped[bool] = mapped_column(Boolean, server_default=true(), nullable=False)
+  is_deleted: Mapped[bool] = mapped_column(Boolean, server_default=false(), nullable=False)
 
   # Relationships
   reservations: Mapped[List["Reservation"]] = relationship("Reservation", back_populates="user", lazy="raise")
