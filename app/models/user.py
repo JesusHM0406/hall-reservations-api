f88c5376a28eccd36fd39cdcdecbfcb1b86d985d
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-22 17:42:26 -0600

    feat(models): expand username max length

--- app/models/user.py
+++ app/models/user.py
@@ -13,7 +13,10 @@ class User(Base):
   __tablename__ = "users"
 
   id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-  name: Mapped[str] = mapped_column(String(30), unique=True, nullable=False)
+  # The maximum name length in the database must be greater than the maximum
+  # length used in validations because when the user is deleted, their name
+  # is longer because the suffix "_del_timestamp" is added
+  name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
   pw_hash: Mapped[str] = mapped_column(String(255), nullable=False)
   role: Mapped[UserRole] = mapped_column(Enum(UserRole, name="user_role"), default=UserRole.USER, nullable=False)
   is_active: Mapped[bool] = mapped_column(Boolean, server_default=true(), nullable=False)
