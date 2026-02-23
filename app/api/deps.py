3e1b867ea67836347db86aacb9c7960753cd7e4e
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-23 14:21:22 -0600

    refactor: remove unnecessary print

--- app/api/deps.py
+++ app/api/deps.py
@@ -40,9 +40,7 @@ async def get_current_user(
     username = payload.get("sub")
     if username is None:
       raise credentials_exception
-  except PyJWTError as e:
-    print("deps.py get_current_user:")
-    print(e)
+  except PyJWTError:
     raise credentials_exception
 
   user = await crud_get_user_by_name(db=db, name=username)
