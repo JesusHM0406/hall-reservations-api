4794b2c01471fe48398085fac997f91298fe634f
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-25 18:57:13 -0600

    test: add tests to verify that cannot delete the last superadmin and the names are released upon deletion

--- tests/test_api/test_users/test_users_me.py
+++ tests/test_api/test_users/test_users_me.py
@@ -7,7 +7,7 @@ from app.core.messages import ErrorMessages
 from app.main import app
 from app.models.user import User
 from app.models.user_role import UserRole
-from app.schemas.user import UserComplete, UserUpdate
+from app.schemas.user import UserComplete, UserCreate, UserUpdate
 
 class TestGetMe:
   async def test_get_me_success(self, client: AsyncClient):
@@ -309,7 +309,108 @@ class TestsDeleteMe:
 
     assert res.status_code == 204
 
+    await db_session.flush()
+
     assert fake_user.is_deleted is True # Then, the data is updated in the db (if there are no errors after)
+
+  async def test_delete_me_admin(self, client: AsyncClient, db_session: AsyncSession):
+    fake_user = User(
+      name="admin",
+      pw_hash="h",
+      role=UserRole.ADMIN,
+      is_active=True,
+      is_deleted=False
+    )
+    db_session.add(fake_user)
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=fake_user.id,
+      name=fake_user.name,
+      role=fake_user.role,
+      is_active=fake_user.is_active,
+      is_deleted=fake_user.is_deleted
+    )
+
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    res = await client.delete("/users/me")
+
+    assert res.status_code == 204
+
+    await db_session.flush()
+
+    assert fake_user.is_deleted is True # Then, the data is updated in the db (if there are no errors after)
+
+  async def test_delete_me_superadmin(self, client: AsyncClient, db_session: AsyncSession):
+    fake_superadmin1 = User(
+      name="superadmin1",
+      pw_hash="h",
+      role=UserRole.SUPERADMIN,
+      is_active=True,
+      is_deleted=False
+    )
+    fake_superadmin2 = User(
+      name="superadmin2",
+      pw_hash="h",
+      role=UserRole.SUPERADMIN,
+      is_active=True,
+      is_deleted=False
+    )
+    db_session.add_all([fake_superadmin1, fake_superadmin2])
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=fake_superadmin1.id,
+      name=fake_superadmin1.name,
+      role=fake_superadmin1.role,
+      is_active=fake_superadmin1.is_active,
+      is_deleted=fake_superadmin1.is_deleted
+    )
+
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    res = await client.delete("/users/me")
+
+    assert res.status_code == 204
+
+    await db_session.flush()
+
+    assert fake_superadmin1.is_deleted is True # Then, the data is updated in the db (if there are no errors after)
+
+  async def test_delete_me_last_superadmin(self, client: AsyncClient, db_session: AsyncSession):
+    fake_superadmin = User(
+      name="superadmin",
+      pw_hash="h",
+      role=UserRole.SUPERADMIN,
+      is_active=True,
+      is_deleted=False
+    )
+    db_session.add(fake_superadmin)
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=fake_superadmin.id,
+      name=fake_superadmin.name,
+      role=fake_superadmin.role,
+      is_active=fake_superadmin.is_active,
+      is_deleted=fake_superadmin.is_deleted
+    )
+
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    res = await client.delete("/users/me")
+
+    assert res.status_code == 400
+    assert res.json()["message"] == ErrorMessages.DELETE_LAST_SUPERADMIN
+
+    await db_session.flush()
+
+    superadmin = await db_session.get(User, fake_superadmin.id)
+
+    assert superadmin is not None
+    assert superadmin.name == "superadmin"
+    assert superadmin.is_deleted is False
 
   async def test_delete_me_deleted(self, client: AsyncClient, db_session: AsyncSession):
     fake_user = User(
@@ -361,6 +462,54 @@ class TestsDeleteMe:
 
     res = await client.delete("/users/me")
 
+    await db_session.flush()
+
     assert res.status_code == 400
     assert res.json()["detail"] == ErrorMessages.INACTIVE_USER
     assert fake_user.is_deleted is False
+
+  # IMPORTANT TEST: implement a tests that first deletes an user successfully and then,
+  # that user tries to access GET /users/me, this should return 404 NOT FOUND
+  # for this tests i need to use real JWT tokens
+
+  async def test_delete_me_name_liberation(self, client: AsyncClient, db_session: AsyncSession):
+    name = "user"
+
+    fake_user = User(
+      name=name,
+      pw_hash="h",
+      role=UserRole.USER,
+      is_active=True,
+      is_deleted=False
+    )
+    db_session.add(fake_user)
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=fake_user.id,
+      name=fake_user.name,
+      role=fake_user.role,
+      is_active=fake_user.is_active,
+      is_deleted=fake_user.is_deleted
+    )
+
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    new_user_scheme = UserCreate(
+      name=name,
+      password="password123",
+      password_confirm="password123"
+    )
+
+    res = await client.post("/users/", json=new_user_scheme.model_dump())
+    assert res.status_code == 409
+
+    res = await client.delete("/users/me")
+    assert res.status_code == 204
+
+    await db_session.flush()
+
+    assert fake_user.is_deleted is True
+
+    res = await client.post("/users/", json=new_user_scheme.model_dump())
+    assert res.status_code == 201
