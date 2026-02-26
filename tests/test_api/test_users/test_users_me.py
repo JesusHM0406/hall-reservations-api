d10dbb5302326d238212e50e29882be98f279370
Jesus HM <jesus.h.mora0406@gmail.com>
2026-02-25 18:18:30 -0600

    test: add necessary tests for the endpoint PATCH /users/me

--- tests/test_api/test_users/test_users_me.py
+++ tests/test_api/test_users/test_users_me.py
@@ -100,6 +100,34 @@ class TestUpdateMe:
     assert user_db is not None
     assert user_db.id == fake_db_user.id
 
+  async def test_update_user_same_name(self, client: AsyncClient, db_session: AsyncSession):
+    fake_db_user = User(name="user", role=UserRole.USER, is_active=True, pw_hash="h")
+    db_session.add(fake_db_user)
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=fake_db_user.id,
+      name=fake_db_user.name,
+      role=fake_db_user.role,
+      is_active=fake_db_user.is_active,
+      is_deleted=False
+    )
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    user_update = UserUpdate(name=fake_db_user.name)
+
+    response = await client.patch("/users/me", json=user_update.model_dump())
+
+    assert response.status_code == 200
+    # The same name, no errors
+    assert response.json()["name"] == fake_db_user.name
+
+    user_res = await db_session.execute(select(User).where(User.name == fake_db_user.name))
+    user_db = user_res.scalar_one_or_none()
+
+    assert user_db is not None
+    assert user_db.id == fake_db_user.id
+
   async def test_update_me_unauthenticated(self, client: AsyncClient):
     app.dependency_overrides = {}
 
@@ -127,6 +155,25 @@ class TestUpdateMe:
     assert response.status_code == 400
     assert response.json()["detail"] == ErrorMessages.INACTIVE_USER
 
+  async def test_update_me_deleted(self, client: AsyncClient, db_session: AsyncSession):
+    fake_db_user = User(name="user", role=UserRole.USER, is_active=False, pw_hash="h", is_deleted=True)
+    db_session.add(fake_db_user)
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=fake_db_user.id,
+      name=fake_db_user.name,
+      role=fake_db_user.role,
+      is_active=fake_db_user.is_active,
+      is_deleted=fake_db_user.is_deleted
+    )
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    response = await client.patch("/users/me")
+
+    assert response.status_code == 404
+    assert response.json()["detail"] == ErrorMessages.USER_NOT_FOUND
+
   async def test_update_me_duplicate_name(self, client: AsyncClient, db_session: AsyncSession):
     user1 = User(name="user1", role=UserRole.USER, is_active=True, pw_hash="h")
     user2 = User(name="user2", role=UserRole.USER, is_active=True, pw_hash="h")
@@ -142,12 +189,99 @@ class TestUpdateMe:
     )
     app.dependency_overrides[get_current_user] = lambda: user_mock
 
-    user_update = UserUpdate(name="user2")
+    user_update = UserUpdate(name=user2.name)
 
     response = await client.patch("/users/me", json=user_update.model_dump())
 
     assert response.status_code == 409
     assert response.json()["message"] == ErrorMessages.DUPLICATED_USERNAME
+
+  async def test_update_me_empty_name(self, client: AsyncClient, db_session: AsyncSession):
+    user1 = User(name="user1", role=UserRole.USER, is_active=True, pw_hash="h")
+    db_session.add(user1)
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=user1.id,
+      name=user1.name,
+      role=user1.role,
+      is_active=user1.is_active,
+      is_deleted=user1.is_deleted
+    )
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    user_update = UserUpdate(name="    ")
+
+    response = await client.patch("/users/me", json=user_update.model_dump())
+
+    assert response.status_code == 400
+    assert response.json()["message"] == ErrorMessages.EMPTY_NAME
+
+    await db_session.flush()
+
+    user = await db_session.get(User, user1.id)
+
+    assert user is not None
+    assert user.name == "user1"
+
+  async def test_update_me_white_space(self, client: AsyncClient, db_session: AsyncSession):
+    user1 = User(name="user1", role=UserRole.USER, is_active=True, pw_hash="h")
+    db_session.add(user1)
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=user1.id,
+      name=user1.name,
+      role=user1.role,
+      is_active=user1.is_active,
+      is_deleted=user1.is_deleted
+    )
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    user_update = UserUpdate(name="  newname ")
+
+    response = await client.patch("/users/me", json=user_update.model_dump())
+
+    assert response.status_code == 200
+
+    await db_session.flush()
+
+    user = await db_session.get(User, user1.id)
+
+    assert user is not None
+    assert user.name == "newname"
+
+  async def test_update_me_role_injection(self, client: AsyncClient, db_session: AsyncSession):
+    user1 = User(name="user1", role=UserRole.USER, is_active=True, pw_hash="h")
+    db_session.add(user1)
+    await db_session.flush()
+
+    user_mock = UserComplete(
+      id=user1.id,
+      name=user1.name,
+      role=user1.role,
+      is_active=user1.is_active,
+      is_deleted=user1.is_deleted
+    )
+    app.dependency_overrides[get_current_user] = lambda: user_mock
+
+    # IMPORTANT: This may fail for other reason if i update the UserUpdate scheme
+    user_update = {
+      "name": "newname",
+      "role": UserRole.SUPERADMIN
+    }
+
+    response = await client.patch("/users/me", json=user_update)
+
+    assert response.status_code == 200
+
+    await db_session.flush()
+
+    user = await db_session.get(User, user1.id)
+
+    assert user is not None
+    assert user.name == "newname"
+    assert user.role == UserRole.USER
 
 class TestsDeleteMe:
   async def test_delete_me_success(self, client: AsyncClient, db_session: AsyncSession):
